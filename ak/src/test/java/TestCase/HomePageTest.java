595abc7a93fe59dd2feee9d3d125a9ab28146a4f
AKGit01 <singhamit2k4@gmail.com>
2024-07-24 13:55:20 +0530

    wait

--- ak/src/test/java/TestCase/HomePageTest.java
+++ ak/src/test/java/TestCase/HomePageTest.java
@@ -1,7 +1,10 @@
 package TestCase;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -9,6 +12,8 @@ import org.testng.annotations.Test;
 import pages.HomePage;
 
 import static org.testng.Assert.assertTrue;
+
+import java.time.Duration;
 
 public class HomePageTest {
     WebDriver driver;
@@ -32,11 +37,16 @@ public class HomePageTest {
     @Test
     public void testSearch() {
         // Enter search text and click the search button
-        homePage.enterSearchText("Selenium WebDriver");
-        homePage.clickSearchButton();
+       // homePage.enterSearchText("Selenium WebDriver");
+        //homePage.clickSearchButton();
         // Verify that the page title contains "Selenium WebDriver"
-        String pageTitle = homePage.getPageTitle();
-        assertTrue(pageTitle.contains("Selenium WebDriver"), "Page title does not contain 'Selenium WebDriver'");
+       // String pageTitle = homePage.getPageTitle();
+     // Create an instance of WebDriverWait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));      
+        // Wait until the element is visible
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("APjFqb"))).sendKeys("Selenium WebDriver");
+        homePage.clickSearchButton();
+        assertTrue(driver.getTitle().contains("Selenium WebDriver"), "Page title does not contain 'Selenium WebDriver'");
     }
 
     @AfterClass
